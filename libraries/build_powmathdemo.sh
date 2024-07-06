c275c8631a8d10c398c938e37af1974a787537b2
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2024-07-06 20:23:32 +0530

    using power form math lib

---
+++ libraries/build_powmathdemo.sh
@@ -0,0 +1,1 @@
+gcc -o powmathdemo powmathdemo.c -lm
---
+++ libraries/powmathdemo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double base = 2.02, exp = 5.04;
+    printf("%f power %f = %f\n", base, exp, pow(base, exp));
+}
