d41f3fdafb8b8b47b049e13c37527f7249fd5260
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2024-09-07 12:40:46 +0530

    Add some menu items

--- data_processing/cricdata_process.py
+++ data_processing/cricdata_process.py
@@ -4,3 +4,31 @@ while True:
     print('\n\t\t\t2. Data Visualization')
     print('\n\t\t\t3. Exit\n')
     choice=int(input('Select your choice [1-3] : '))
+    if choice==1:
+        while True:
+            print('\t*** CRICKET DATA ANALYSIS SYSTEM')
+            print('*'*50)
+            print('\n\t\t1. Show Batsmen data')
+            print('\n\t\t2. Show Bowler data')
+            print('\n\t\t3. Exit to main menu\n\n')
+            choice=int(input('Select your choice [1-3] : '))
+            if choice==1:
+                print('\nBatsmen Data:\n\n')
+            elif choice==2:
+                print('\nBowler Data:\n\n')
+            elif choice==3:
+                print('Exit to main menu...')
+                break
+    elif choice==2:
+        while True:
+            print('\t*** CRICKET DATA VISUALIZATION')
+            print('*'*50)
+            print('\n\t\t1. Plot Line Chart (Name and runs)')
+            print('\n\t\t2. Plot Line Chart (Name and wickets)')
+            print('\n\t\t3. Exit the Program\n\n')
+            choice=int(input('Select your choice [1-3] : '))
+    elif choice==3:
+        print('Exit from menu...')
+        break
+    else:
+        print('Invalid choice')
