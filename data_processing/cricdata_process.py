8b42c038b55986f4e39d89077b577203727b2352
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2024-09-07 12:24:01 +0530

    Initial file to process cricket data

---
+++ data_processing/cricdata_process.py
@@ -0,0 +1,6 @@
+while True:
+    print('\t\t *** Cricket Data Analysis Project *** ')
+    print('\n\t\t\t1. Data Analysis')
+    print('\n\t\t\t2. Data Visualization')
+    print('\n\t\t\t3. Exit\n')
+    choice=int(input('Select your choice [1-3] : '))
